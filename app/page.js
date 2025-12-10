c042cfc4c3e7e8c8a096b82f421215224f9bca4a
noah.stanich@edu.sait.ca <noah.stanich@edu.sait.ca>
2025-12-10 13:35:55 -0700

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function SignInPage(){
+    return(
+        <main>
+            <header>
+                <h1>Welcome to Redsky!</h1>
+            </header>
+            <section>
+              <div>
+
+              </div>
+            </section>
+        </main>
+    )
+}
