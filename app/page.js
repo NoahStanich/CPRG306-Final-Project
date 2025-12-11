021e012be258415462ce6056c81fbd6496c7a761
noah.stanich@edu.sait.ca <noah.stanich@edu.sait.ca>
2025-12-11 15:10:16 -0700

    Added profile page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 import Link from "next/link";
 
-export default function SignInPage(){
+export default function MainPage(){
+
+  let navBarStyle = "mt-200 bg-gray-800 pb-5 text-center divide-x";
+
     return(
-        <main>
-            <header>
-                <h1>Welcome to Redsky!</h1>
+        <main className="p-4">
+            <header className="mb-4 text-center">
+                <h1 className="text-3xl font-bold">Welcome to Redsky!</h1>
             </header>
             <section>
-              <div>
-
+              <div className={navBarStyle}>
+                <Link href="profile" className="px-4">Profile</Link>
+                <Link href="post-list" className="px-4">List of posts</Link>
+                <Link href="reply" className="px-4">Reply</Link>
+                <Link href="write-posts" className="px-4">Write a post</Link>
               </div>
             </section>
         </main>
---
+++ app/profile.js
@@ -0,0 +1,14 @@
+export default function Profile(){
+    return(
+        <main>
+            <header className="mt-4 mb-4">
+                <h1 className="text-3xl text-center font-bold">Profile</h1>
+            </header>
+            <section className="flex justify-center">
+              <div>
+                <img src="https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg" className="w-50 h-50 rounded-full" />
+              </div>
+            </section>
+        </main>
+    )
+}
