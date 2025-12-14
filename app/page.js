78e72a8a1ff0fc66cc7f300a3c6af4bf5d73143d
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-14 00:57:56 -0700

    Implimented Authentication

--- app/page.js
+++ app/page.js
@@ -1,22 +1,64 @@
+"use client"
+
 import Link from "next/link";
+
+import { useUserAuth } from "./_utils/auth-context";
+
 
 export default function MainPage(){
 
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+
   let navBarStyle = "mt-200 bg-gray-800 pb-5 text-center divide-x";
 
-    return(
-        <main className="p-4">
-            <header className="mb-4 text-center">
-                <h1 className="text-3xl font-bold">Welcome to Redsky!</h1>
-            </header>
+  async function handleSignIn() {
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleSignOut() {
+    try {
+      await firebaseSignOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return(
+      <main className="p-4">
+        <header className="mb-4 text-center">
+            <h1 className="text-3xl font-bold">Welcome to Redsky!</h1>
+        </header>
+          { user ? (
+            <div>
+              <section>
+                <button type="button" onClick={handleSignOut}>Sign Out</button>
+              </section>
+              <section>
+                <div className={navBarStyle}>
+                  <Link href="profile" className="px-4">Profile</Link>
+                  <Link href="post-list" className="px-4">List of posts</Link>
+                  <Link href="reply" className="px-4">Reply</Link>
+                  <Link href="write-posts" className="px-4">Write a post</Link>
+                </div>
+              </section>
+            </div>
+        ) : (
+          <div>
             <section>
-              <div className={navBarStyle}>
-                <Link href="profile" className="px-4">Profile</Link>
-                <Link href="post-list" className="px-4">List of posts</Link>
-                <Link href="reply" className="px-4">Reply</Link>
-                <Link href="write-posts" className="px-4">Write a post</Link>
+              <div>
+                <p>Welcome!</p>
               </div>
             </section>
-        </main>
-    )
+            <section>
+              <button type="button" onClick={handleSignIn}>Sign In with Github</button>
+            </section>
+          </div>
+        )}
+    </main>
+  )
 }
