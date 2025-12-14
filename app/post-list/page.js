1004bf2230ac22f1b466f6918126902d7e76472b
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-14 11:06:30 -0700

    Base implimentation

--- app/post-list/page.js
+++ app/post-list/page.js
@@ -2,15 +2,32 @@
 
 import PostCompObject from "./post-comp"
 import { useUserAuth } from "../_utils/auth-context";
+import { useEffect, useState } from "react";
+import { dbGetAllBlogPosts } from "../_services/blog-service";
+import Link from "next/link";
+
+
 
 export default function PropsPage(){
-
     const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
+    const [blogPostList, setBlogPostList] = useState([]);
 
+    function handleBlogChange(input){
+        setBlogPostList(input)
+    }
 
+    useEffect(() => {
+        if (user) dbGetAllBlogPosts(user.uid, handleBlogChange);
+    }, [user]);
+    console.log(blogPostList);
+
+    
+
+    /*
     let postOne = {
     id: 1,
+    uid: null,
     dateTime: "December 14 10:00 AM",
     text: "This is a post.",
     photoUrl: ""
@@ -18,18 +35,23 @@ export default function PropsPage(){
 
     let postTwo = {
     id: 2,
+    uid: null,
     dateTime: "December 14 11:00 AM",
     text: "This is also a post.",
     photoUrl: ""
     }
+    */
 
 return (
     <main>
     { user ? (
         <div>
             <h1>List of Posts</h1>
-            <PostCompObject postObj={postOne}/>
-            <PostCompObject postObj={postTwo}/>
+            {blogPostList.map((post) => (
+                <li key={post.id}>
+                    <Link href={`/${post.id}`}>{post.title}</Link>
+                </li>
+            ))}
         </div>
     ) : (
         <div>
