0c2d805d45d6440703987df3db61c78f287fafb5
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-14 01:02:36 -0700

    Implimented protection for list of pages

--- app/post-list/post-comp.js
+++ app/post-list/post-comp.js
@@ -1,10 +1,9 @@
 export default function PostCompObject( {postObj} ){
 
-    let {id, dateTime, text, photoUrl} = postObj;
+    let {id, dateTime, text} = postObj;
 
     return (
         <div className="bg-blue-300 m-4 p-4 rounded-2xl">
-            <img src={photoUrl} alt="image"></img>
             <ul className="list-disc pl-4 mt-4 text-center">
                 <li className="text-md font-bold font-serif">{dateTime}</li>
                 <li className="text-sm">{text}</li>
